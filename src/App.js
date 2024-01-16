97e4f51f1788c8dc49ef1add9f85ec9fee2fa966
Harshit Pradhan <hpradhan020@gmail.com>
2024-01-16 22:26:52 +0530

    component load

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import InputChip from "./components/InputChip/InputChip";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <InputChip />
+    </>
+  );
 }
 
 export default App;
